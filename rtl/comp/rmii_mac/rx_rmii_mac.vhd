9198a22ba86ec1cc5c419c92f4d37cf8f0d13b75
Jakub Cabal <jakubcabal@gmail.com>
2019-09-21 19:21:40 +0200

    WIP changes of RX RMII MAC.

--- rtl/comp/rmii_mac/rx_rmii_mac.vhd
+++ rtl/comp/rmii_mac/rx_rmii_mac.vhd
@@ -71,18 +71,18 @@ begin
     begin
         if (rising_edge(RMII_CLK)) then
             rmii_rxd_meta    <= RMII_RXD;
-            rmii_rxd_reg     <= rmii_rxd_meta;
+            rmii_rxd_sync    <= rmii_rxd_meta;
             rmii_csr_dv_meta <= RMII_CSR_DV;
-            rmii_csr_dv_reg  <= rmii_csr_dv_meta;
+            rmii_csr_dv_sync <= rmii_csr_dv_meta;
         end if;
     end process;
 
     process (RMII_CLK)
     begin
         if (rising_edge(RMII_CLK)) then
-            rmii_rxd_reg2    <= rmii_rxd_reg;
-            rmii_csr_dv_reg2 <= rmii_csr_dv_reg;
-            if (rmii_csr_dv_reg = '1') then
+            rmii_rxd_reg    <= rmii_rxd_sync;
+            rmii_csr_dv_reg <= rmii_csr_dv_sync;
+            if (rmii_csr_dv_sync = '1') then
                 rx_cnt <= rx_cnt + 1;
             else
                 rx_cnt <= (others => '0');
@@ -95,8 +95,8 @@ begin
     process (RMII_CLK)
     begin
         if (rising_edge(RMII_CLK)) then
-            data_reg <= rmii_rxd_reg2 & data_reg(7 downto 2);
-            last_reg <= rmii_csr_dv_reg2 and not rmii_csr_dv_reg;
+            rx_byte <= rmii_rxd_reg & rx_byte(7 downto 2);
+            rx_byte_last <= rmii_csr_dv_reg and not rmii_csr_dv_sync;
         end if;
     end process;
 
@@ -104,9 +104,9 @@ begin
     begin
         if (rising_edge(RMII_CLK)) then
             if (RMII_RST = '1') then
-                vld_reg <= '0';
+                rx_byte_vld <= '0';
             else
-                vld_reg <= rx_cnt_max and rmii_csr_dv_reg2;
+                rx_byte_vld <= rx_cnt_max and rmii_csr_dv_reg;
             end if;
         end if;
     end process;
@@ -126,26 +126,26 @@ begin
         end if;
     end process;
 
-    process (fsm_pstate, vld_reg, data_reg, last_reg)
+    process (fsm_pstate, rx_byte_vld, rx_byte, rx_byte_last)
     begin
-        fsm_nstate   <= idle;
-        vld_flag     <= '0';
-        sop_flag     <= '0';
-        eop_flag     <= '0';
+        fsm_nstate <= idle;
+        vld_flag   <= '0';
+        sop_flag   <= '0';
+        eop_flag   <= '0';
 
         case fsm_pstate is
             when idle =>
-                if (vld_reg = '1' and data_reg = X"55") then
+                if (rx_byte_vld = '1' and rx_byte = X"55") then
                     fsm_nstate <= preamble;
                 else
                     fsm_nstate <= idle;
                 end if;
 
             when preamble => -- todo check number of preamble bytes
-                if (vld_reg = '1') then
-                    if (data_reg = X"D5") then
+                if (rx_byte_vld = '1') then
+                    if (rx_byte = X"D5") then
                         fsm_nstate <= sfd;
-                    elsif (data_reg = X"55") then
+                    elsif (rx_byte = X"55") then
                         fsm_nstate <= preamble;
                     else
                         fsm_nstate <= idle;
@@ -155,7 +155,7 @@ begin
                 end if;
 
             when sfd => -- start frame delimiter
-                if (vld_reg = '1') then
+                if (rx_byte_vld = '1') then
                     fsm_nstate <= sop;
                 else
                     fsm_nstate <= sfd;
@@ -164,7 +164,7 @@ begin
             when sop => -- start of packet (first byte)
                 vld_flag <= '1';
                 sop_flag <= '1';
-                if (vld_reg = '1') then
+                if (rx_byte_vld = '1') then
                     fsm_nstate <= wait4eop;
                 else
                     fsm_nstate <= sfd;
@@ -172,7 +172,7 @@ begin
 
             when wait4eop => -- wait for end of packet (last byte)
                 vld_flag <= '1';
-                if (vld_reg = '1' and last_reg = '1') then
+                if (rx_byte_vld = '1' and rx_byte_last = '1') then
                     eop_flag   <= '1';
                     fsm_nstate <= idle;
                 else
@@ -185,9 +185,9 @@ begin
     process (RMII_CLK)
     begin
         if (rising_edge(RMII_CLK)) then
-            data_reg2 <= data_reg;
-            sop_reg2  <= sop_flag;
-            eop_reg2  <= eop_flag;
+            sb0_data <= rx_byte;
+            sb0_sop  <= sop_flag;
+            sb0_eop  <= eop_flag;
         end if;
     end process;
 
@@ -195,11 +195,64 @@ begin
     begin
         if (rising_edge(RMII_CLK)) then
             if (RMII_RST = '1') then
-                vld_reg2 <= '0';
+                sb0_vld <= '0';
             else
-                vld_reg2 <= vld_reg and vld_flag;
+                sb0_vld <= rx_byte_vld and vld_flag;
             end if;
         end if;
     end process;
+
+    -- -------------------------------------------------------------------------
+    --  FRAME CHECK AND STATISTICS
+    -- -------------------------------------------------------------------------
+
+    process (RMII_CLK)
+    begin
+        if (rising_edge(RMII_CLK)) then
+            if (RMII_RST = '1') then
+                rx_pkt_cnt <= (others => '0');
+            elsif (sb0_sop = '1' and sb0_vld = '1') then
+                rx_pkt_cnt <= rx_pkt_cnt + 1;
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  STORE AND FORWARD/DISCARD FIFO
+    -- -------------------------------------------------------------------------
+
+    fifom_din  <= sb1_data & sb1_sop & sb1_eop;
+    fifom_mark <= sb1_sop and sb1_vld;
+    --fifom_discard <= fifom_full; TODO
+
+    fifo_mark_i: entity work.FIFO_MARK
+    generic map (
+        DATA_WIDTH => 8+1+1, -- data + sop + eop
+        ADDR_WIDTH => 11 -- fifo depth = 2048 - 1 words
+    )
+    port map (
+        CLK      => RMII_CLK,
+        RST      => RMII_RST,
+        -- FIFO WRITE INTERFACE
+        DIN      => fifom_din,
+        WR_EN    => sb1_vld,
+        MARK     => fifom_mark,
+        DISCARD  => fifom_discard,
+        FULL     => fifom_full,
+        -- FIFO READ INTERFACE
+        DOUT     => fifom_dout,
+        DOUT_VLD => sb2_vld,
+        RD_EN    => sb2_rdy,
+        -- FIFO STATUS SIGNAL
+        STATUS   => fifom_status
+    );
+
+    sb2_data <= fifom_dout(8+2-1 downto 2);
+    sb2_sop  <= fifom_dout(1);
+    sb2_eop  <= fifom_dout(0);
+
+    -- -------------------------------------------------------------------------
+    --  CROSS DOMAIN CROSSING FIFO
+    -- -------------------------------------------------------------------------
 
 end architecture;
