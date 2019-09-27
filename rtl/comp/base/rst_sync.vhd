2aec52b9a7719d721b0b84d0779b72c4f033a711
Jakub Cabal <jakubcabal@gmail.com>
2019-09-27 13:40:41 +0200

    Added constraints to RST_SYNC.

--- rtl/comp/base/rst_sync.vhd
+++ rtl/comp/base/rst_sync.vhd
@@ -19,8 +19,17 @@ end entity;
 
 architecture RTL of RST_SYNC is
 
+    attribute ALTERA_ATTRIBUTE : string;
+    attribute PRESERVE         : boolean;
+
     signal meta_reg  : std_logic;
     signal reset_reg : std_logic;
+
+    attribute ALTERA_ATTRIBUTE of RTL : architecture is "-name SDC_STATEMENT ""set_false_path -to [get_registers {*RST_SYNC:*|meta_reg}] """;
+    attribute ALTERA_ATTRIBUTE of meta_reg  : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+    attribute ALTERA_ATTRIBUTE of reset_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+    attribute PRESERVE of meta_reg      : signal is TRUE;
+    attribute PRESERVE of reset_reg     : signal is TRUE;
 
 begin
 
